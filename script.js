93611935b90dc632ee83d365d7eb0c47745ea437
Bruno Fernandes <brunofernandesf.gd@gmail.com>
2024-01-05 10:18:43 -0300

    ajuste da estilização

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ document.querySelector('.cabecalho__opcoes__modo__ponto').addEventListener('clic
     e.target.classList.toggle('cabecalho__opcoes__modo__ponto-move');
     document.body.classList.toggle('dark');
 });
+
+var menuItems = document.querySelectorAll('.menu__lista__nav');
+
+menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener('click', function () {
+
+        menuItems.forEach(function (item) {
+            item.classList.remove('active');
+        });
+
+        menuItem.classList.add('active');
+    });
+});
